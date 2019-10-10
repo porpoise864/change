cbc4468351858521fde6fd893ff3a953b4394be8
porpoise864 <oertlys@oregonstate.edu>
2019-10-09 22:41:29 -0400

    new version of change.py

--- change.py
+++ change.py
@@ -1,12 +1,13 @@
 # Author: Sarah Oertly
 # Date: 10/8/2019
-# Description: The program will prompt the user to ask for an integer number of cents from 0 to 99.
-# The program calculates and outputs the fewest total number of coins required to get those cent amount.
+# Description: Write a program that asks the user for a (integer) number of cents, from 0 to
+# 99, and outputs how many of each type of coin would represent that amount with the fewest total number
+# of coins. When you run your program, it should match the
+# following format:
 print("Please enter an amount in cents less than a dollar.")
-amount = float(input())
-Q = (amount % 25) + ((amount % 25)-(amount % 25))
-D = (amount % 25) / 10 - (amount % 25) / 10
-N = (amount % 25) / 5
-P = ((amount % 25) / 5) - 1
-
-print("Your change will be: ", "Q: ", Q, "D: ", D, "N: ", N, "P: ", P)
+amount = int(input())
+Q = (amount / 25) % 25
+D = (amount % 25) % Q
+N = (D % 10) / 5
+P = (N % 5) % (N / 5)
+print("Your change will be:\n ","Q:", Q, "\nD: ", D, "\nN: ", N, "\nP: ", P)
